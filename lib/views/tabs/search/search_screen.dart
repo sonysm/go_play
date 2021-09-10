87d065fd284e20d16ff065fc431f427666d03ef4
viseth <sokunviseth@gmail.com>
2021-09-10 13:14:45 +0700

    updated

--- lib/views/tabs/search/search_screen.dart
+++ lib/views/tabs/search/search_screen.dart
@@ -14,7 +14,6 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-
   TextEditingController _textController = TextEditingController();
   late FocusNode _searchFocusScop;
   bool? _isSearched;
@@ -22,8 +21,40 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          elevation: 0,
+          titleSpacing: 0,
+          title: Container(
+            height: 38,
+            margin: const EdgeInsets.only(right: 16.0),
+            child: TextField(
+              controller: _textController,
+              focusNode: _searchFocusScop,
+              onSubmitted: onSearch,
+              textInputAction: TextInputAction.search,
+              style: Theme.of(context).textTheme.bodyText1?.copyWith(fontFamily: 'OpenSans'),
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade500,
+                  ),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(50), borderSide: BorderSide.none),
+                  hintStyle: TextStyle(fontSize: 14, color: Colors.grey.shade500),
+                  hintText: "Search users"),
+            ),
+          ),
+        ),
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Container(
+            color: Colors.transparent,
+          ),
+        )
+        /*SafeArea(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
           child: Container(
@@ -32,7 +63,8 @@ class _SearchScreenState extends State<SearchScreen> {
               children: [
                 Container(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-                  decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 0.5, color: isLight(context) ?  Colors.black12 : Colors.white12))),
+                  decoration:
+                      BoxDecoration(border: Border(bottom: BorderSide(width: 0.5, color: isLight(context) ? Colors.black12 : Colors.white12))),
                   child: Row(
                     children: [
                       CupertinoButton(
@@ -57,7 +89,8 @@ class _SearchScreenState extends State<SearchScreen> {
                       Expanded(
                         child: Container(
                           height: 44.0,
-                          decoration: BoxDecoration(color: isLight(context) ? Colors.blueGrey[50] : Colors.blueGrey, borderRadius: BorderRadius.circular(50)),
+                          decoration:
+                              BoxDecoration(color: isLight(context) ? Colors.blueGrey[50] : Colors.blueGrey, borderRadius: BorderRadius.circular(50)),
                           child: TextField(
                             controller: _textController,
                             focusNode: _searchFocusScop,
@@ -81,8 +114,8 @@ class _SearchScreenState extends State<SearchScreen> {
             ),
           ),
         ),
-      ),
-    );
+      ),*/
+        );
   }
 
   @override
